2a36eba7e88e372ad704a360badf52e378fa7fb3
zyzo <danghaian168@gmail.com>
2015-06-24 12:45:56 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+Welcome to feed.api.fossasia.net ! <br/>
+
+The project's source code can be found at <a href="https://github.com/fossasia/feed.api.fossasia.net">fossasia/feed.api.fossasia.net</a> on Github.
